b3bbbd00fb26650521c8bcc7ab2c4a5b2f76b064
Vsevolod Slavianinov <a0974377355@gmail.com>
2022-11-30 18:18:32 +0200

    start page added. charts added. recharts installed.

--- src/components/index.js
+++ src/components/index.js
@@ -8,5 +8,10 @@ import JobsContainer from './JobsContainer';
 import Job from './Job';
 import SearchContainer from './SearchContainer';
 import Loading from './Loading';
+import ChartsContainer from './ChartsContainer';
+import StatsContainer from './StatsContainer';
+import StatItem from './StatItem';
+import AreaChartComponent from './AreaChart';
+import BarChartComponent from './BarChart';
 
-export { Logo, Navbar, BigSidebar, SmallSidebar, FormRow, FormRowSelect, JobsContainer, Job, SearchContainer, Loading };
+export { Logo, AreaChartComponent, BarChartComponent, StatItem, Navbar, BigSidebar, SmallSidebar, FormRow, FormRowSelect, JobsContainer, Job, SearchContainer, Loading, ChartsContainer, StatsContainer };
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -40,6 +40,21 @@ export const getAllJobs = createAsyncThunk(
     }
 );
 
+export const showStats = createAsyncThunk(
+    'allJobs/showStats',
+    async (_, thunkAPI) => {
+        try {
+            const resp = await customFetch.get('/jobs/stats', {
+                headers: {
+                    authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+                },
+            });
+            return resp.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.response.data.msg);
+        }
+    }
+);
 
 const allJobsSlice = createSlice({
     name: 'allJobs',
@@ -62,6 +77,18 @@ const allJobsSlice = createSlice({
                 state.jobs = payload.jobs;
             })
             .addCase(getAllJobs.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                toast.error(payload);
+            })
+            .addCase(showStats.pending, state => {
+                state.isLoading = true;
+            })
+            .addCase(showStats.fulfilled, (state, { payload }) => {
+                state.isLoading = false;
+                state.stats = payload.defaultStats;
+                state.monthlyApplications = payload.monthlyApplications;
+            })
+            .addCase(showStats.rejected, (state, { payload }) => {
                 state.isLoading = false;
                 toast.error(payload);
             });
